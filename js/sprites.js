d4c8401d648f530e6716384266fdda84acc96433
박정인 <jeongin@hivelab.co.kr>
2024-08-06 18:36:37 +0900

    feat: create sprite module

---
+++ js/sprites.js
@@ -0,0 +1,104 @@
+var Example = Example || {};
+
+Example.sprites = function() {
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Composites = Matter.Composites,
+        Common = Matter.Common,
+        MouseConstraint = Matter.MouseConstraint,
+        Mouse = Matter.Mouse,
+        Composite = Matter.Composite,
+        Bodies = Matter.Bodies;
+
+    // 엔진 생성
+    var engine = Engine.create(),
+        world = engine.world;
+
+    // 렌더러 생성
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showAngleIndicator: false,
+            wireframes: false
+        }
+    });
+
+    Render.run(render);
+
+    // runner 생성
+    var runner = Runner.create();
+    Runner.run(runner, engine);
+
+    // 바디들 추가
+    var offset = 10,
+        options = { 
+            isStatic: true
+        };
+
+    world.bodies = [];
+
+    // these static walls will not be rendered in this sprites example, see options
+    Composite.add(world, [
+        Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, options),
+        Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, options),
+        Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, options),
+        Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, options)
+    ]);
+
+    var stack = Composites.stack(20, 20, 10, 4, 0, 0, function(x, y) {
+        if (Common.random() > 0.35) {
+            return Bodies.rectangle(x, y, 64, 64, {
+                render: {
+                    strokeStyle: '#ffffff',
+                    sprite: {
+                        texture: '../assets/img/puppy.png'
+                    }
+                }
+            });
+        } else {
+            return Bodies.circle(x, y, 46, {
+                density: 0.0005,
+                frictionAir: 0.06,
+                restitution: 0.3,
+                friction: 0.01,
+                render: {
+                    sprite: {
+                        texture: '../assets/img/puppy.png'
+                    }
+                }
+            });
+        }
+    });
+
+    Composite.add(world, stack);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    Composite.add(world, mouseConstraint);
+
+    // 마우스를 렌더링과 동기화
+    render.mouse = mouse;
+
+    // 뷰포트 화면 설정
+    Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: 800, y: 600 }
+    });
+
+};
+
+Example.sprites();
